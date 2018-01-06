16b5bc09ee1ed1637a8ff2d29e11143db17e2aaa
Brock Gibson <bngd58@mail.missouri.edu>
2018-01-06 17:34:05 -0600

    added parsing for adding and removing bots and added roles to a array

--- RoleManager.js
+++ RoleManager.js
@@ -2,17 +2,29 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const config = require('./config.json');
 
+var roles = [];
+
 client.on('ready', (e) => {
-  console.log('Hello World');
+  console.log('Discord Bot is up and ready!');
 });
 
 client.on('message', message => {
 
-  if (!message.content.startsWith(config.prefix)) return;
+    var args = message.content.split(' ');
 
-  if(message.content === config.prefix + 'hello'){
-    message.channel.send('Salutations ' + message.author.username + '!');
-  }
+    if (!message.content.startsWith(config.prefix)) return;
+
+    if(message.content.startsWith(config.prefix + 'add role'){
+        for (var i = 2, i < args.length; i++){
+            roles.push(args[i]);
+        }
+    }
+    else if(message.content.startsWith(config.prefix + 'remove role'){
+        for (var i = 2, i < args.length; i++){
+            roles.push(args[i]);
+        }
+    }
+
 });
 
 client.login(config.key);
